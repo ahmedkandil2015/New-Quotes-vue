361a4821b49b7944de4df058cb86fc9c448a9068
Ahmed Kandil <ahmedkandil2015@hotmail.com>
2018-06-22 22:37:21 +0200

    create quotes project in vuejs

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <prograss></prograss> 
+  <div class="row">
+  </div>
+   
+</div>
+
+</template>
+
+<script>
+import prograss from './components/Prograssbar.vue';
+
+export default {
+
+components:{
+  Prograss:prograss,
+}
+}
+</script>
+
+<style scoped>
+div{
+  max-width: 90%;
+  margin:0 auto;
+}
+</style>
